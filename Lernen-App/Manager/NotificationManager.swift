5a7d8c00b9761e5fdd575f14cd10477f57df6630
danielandrewr <danielandrewtt@gmail.com>
2022-07-29 10:33:47 +0700

    Added createPathReminder to schedule reminder notification

--- Lernen-App/Manager/NotificationManager.swift
+++ Lernen-App/Manager/NotificationManager.swift
@@ -31,4 +31,20 @@ class NotificationManager {
         let requestNotification = UNNotificationRequest(identifier: id, content: content, trigger: trigger)
         notificationCenter.add(requestNotification)
     }
+    
+    public func createPathReminder(pathName: String, pathDate: Date, minuteInterval: Double) {
+        let content = UNMutableNotificationContent()
+        content.title = "\(pathName) is starting soon 🔥"
+        content.body = "Get yourself ready before hopping in to learning session!"
+        
+        let triggerDate = pathDate.addingTimeInterval(Double(-(minuteInterval * 60)))
+        let calendar = Calendar.current
+        let trigger = UNCalendarNotificationTrigger(
+            dateMatching: calendar.dateComponents([.timeZone, .year, .month, .day, .hour, .minute, .second],
+            from: triggerDate),
+            repeats: false
+        )
+        
+        self.scheduleNotification(id: UUID().uuidString, content: content, trigger: trigger)
+    }
 }
